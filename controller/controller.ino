4bcf5e22249c5548f36e285813d53f3fff9d69ba
tiffany <tngai@uwaterloo.ca>
2023-07-08 22:22:16 -0400

    clean up code for testing

--- controller/controller.ino
+++ controller/controller.ino
@@ -11,8 +11,6 @@ double latitude, longtitude, tilt_angle;
 
 // Motor
 const double motor_step_ratio = 2048.0 / 360.0;
-const int motor_interrupt_pin = 8;  // Connect to interrupt pin
-volatile bool motorState = false;  // Motor state (ON/OFF)
 AccelStepper stepper(AccelStepper::FULL4WIRE, 2, 4, 3, 5);
 const int angle_address = 0;
 double current_angle = 0.0;
@@ -25,7 +23,7 @@ int daylight_start_min;
 
 
 // LED
-const int LED_pin = 13; //TODO: tbd
+const int LED_pin = 13;
 
 // Current sensor
 Adafruit_INA219 ina219;
@@ -35,6 +33,33 @@ float current_mA = 0;
 float voltage = 0;
 float power_mW = 0;
 
+void move_motor(){
+  time_t current_time = now();
+
+  int current_hour = hour(current_time);
+  int current_minute = minute(current_time);
+  int current_second = second(current_time);
+
+  previous_t_sec = current_hour * 3600 + current_minute * 60 + current_second;
+  
+  double x = current_hour * 60 + current_minute - daylight_start_min;
+  rotation_angle = fitted_m * x + fitted_b;
+  if (rotation_angle > 90){ 
+    rotation_angle = 90;
+  }
+  else if (rotation_angle < -90) {
+    rotation_angle = -90;
+  }
+  motor_step = rotation_angle * motor_step_ratio;
+  current_angle = EEPROM.read(angle_address);
+  stepper.moveTo(motor_step);
+  while (stepper.distanceToGo() != 0) {
+    stepper.run();
+  }
+  stepper.disableOutputs();
+  current_angle = stepper.currentPosition() / motor_step_ratio;
+  EEPROM.write(angle_address, current_angle);
+}
  
 void setup(){
   Serial.begin(115200);
@@ -100,47 +125,9 @@ void setup(){
       start_second = start_time.substring(17, 19).toInt();
 
       setTime(start_hour, start_minute, start_second, start_day, start_month, start_year);
-
-//      //TODO: delete section after testing
-//      Serial.print("Current time: ");
-//      Serial.println(start_time);
-//      Serial.print("Month: ");
-//      Serial.println(start_month);
-//      Serial.print("Day: ");
-//      Serial.println(start_day);
-//      Serial.print("Year: ");
-//      Serial.println(start_year);
-//      Serial.print("Hour: ");
-//      Serial.println(start_hour);
-//      Serial.print("Minute: ");
-//      Serial.println(start_minute);
-//      Serial.print("Second: ");
-//      Serial.println(start_second);
-//      Serial.print("Timezone: ");
-//      Serial.println(time_zone);
-//      Serial.print("Latitude: ");
-//      Serial.println(latitude);
-//      Serial.print("Longtitude: ");
-//      Serial.println(longtitude);
-//      Serial.print("Tilt angle: ");
-//      Serial.println(tilt_angle);
-//      Serial.print("Daylight start min: ");
-//      Serial.println(daylight_start_min);
-//      Serial.print("Fitted m: ");
-//      Serial.println(fitted_m);
-//      Serial.print("Fitted b: ");
-//      Serial.println(fitted_b);
     }  
   }
-
-  
-  time_t current_time = now();
-
-  int current_hour = hour(current_time);
-  int current_minute = minute(current_time);
-  int current_second = second(current_time);
-
-  previous_t_sec = current_hour * 3600 + current_minute * 60 + current_second;
+  move_motor();
 }
 
 void loop(){
@@ -152,92 +139,19 @@ void loop(){
   
   current_t_sec = current_hour * 3600 + current_minute * 60 + current_second;
 
-//  rotation_angle = 180.5;
-//  double motor_step = rotation_angle * motor_step_ratio;
-//
-//  current_angle = rotation_angle;
-//  EEPROM.put(angle_address, current_angle);
-//  EEPROM.end();
-//  
-//  if (stepper.distanceToGo() == 0) {
-//    stepper.stop();
-//  }
-//  else {
-//    stepper.run();
-//    current_angle = stepper.currentPosition() / motor_step_ratio;
-//    EEPROM.put(angle_address, current_angle);
-//    EEPROM.end();
-//  }
-
-  //testing motor
-  moving_interval_sec = 5;
-  if (current_t_sec - previous_t_sec >= moving_interval_sec) {
-    double x = hour() * 60 + minute() - daylight_start_min;
-    rotation_angle = fitted_m * x + fitted_b;
-    if (rotation_angle > 90){ 
-      rotation_angle = 90;
-    }
-    else if (rotation_angle < -90) {
-      rotation_angle = -90;
-    }
-    rotation_angle = 90;
-    motor_step = rotation_angle * motor_step_ratio;
-    current_angle = EEPROM.read(angle_address);
-    Serial.print("motor_step_ratio: "); Serial.println(motor_step_ratio);
-    Serial.print("current_angle: "); Serial.println(current_angle);
-    Serial.print("rotation_angle: "); Serial.println(rotation_angle);
-    Serial.print("motor_step: "); Serial.println(motor_step);
-    stepper.moveTo(motor_step);
-    while (stepper.distanceToGo() != 0) {
-      stepper.run();
-    }
-    stepper.disableOutputs();
-    current_angle = stepper.currentPosition() / motor_step_ratio;
-    EEPROM.write(angle_address, current_angle);
+  // 6am to 8pm - comment the following for fixed axis
+  if ((current_hour >= 6 && current_hour < 20) && (current_t_sec - previous_t_sec >= moving_interval_sec)) {    
+    move_motor();
     previous_t_sec = current_t_sec;
   }
 
-//  Serial.println("");
-//  Serial.print("Current time: ");
-//  Serial.print(current_hour);
-//  Serial.print(":");
-//  Serial.print(current_minute);
-//  Serial.print(":");
-//  Serial.print(current_second);
-//  double x = current_hour * 60 + current_minute - daylight_start_min;
-//  rotation_angle = fitted_m * x + fitted_b;
-//  Serial.println("");
-//  Serial.print("Input minute: ");
-//  Serial.print(x);
-//  Serial.println("");
-//  Serial.print("Rotational angle: ");
-//  Serial.print(rotation_angle);
-  
-
-  // 6am to 8pm
-  if ((current_hour >= 6 && current_hour < 20) && (current_t_sec - previous_t_sec >= moving_interval_sec)) {
-    double x = hour() * 60 + minute() - daylight_start_min;
-    rotation_angle = fitted_m * x + fitted_b;
-    if (rotation_angle > 90){ 
-      rotation_angle = 90;
-    }
-    else if (rotation_angle < -90) {
-      rotation_angle = -90;
-    }
-//    motor_step = rotation_angle * motor_step_ratio;
-//    stepper.moveTo(motor_step);
-    previous_t_sec = current_t_sec;
+  // 8pm to 6am
+  if (current_hour >= 20 || current_hour < 6) {
+    digitalWrite(LED_pin, HIGH);
   }
-
-//  // 8pm to 6am
-//  if (current_hour >= 20 || current_hour < 6) {
-//    digitalWrite(LED_pin, HIGH);
-//  }
-//  else{
-//    digitalWrite(LED_pin, LOW);
-//  }
-
-//  delay(5000);
+  else{
+    digitalWrite(LED_pin, LOW);
+  }
 
 
   // Measure power
@@ -253,11 +167,10 @@ void loop(){
   
   power_mW = (0.7*voltage)*current_mA;
   
-//  Serial.print(current_hour); Serial.print(":"); 
-//  Serial.print(current_minute); Serial.print(":"); 
-//  Serial.print(current_second); Serial.print(", "); 
-//  Serial.print(voltage); Serial.print(", "); 
-//  Serial.print(current_mA); Serial.print(", ");
-//  Serial.println(power_mW);
-//  
+  Serial.print(current_hour); Serial.print(":"); 
+  Serial.print(current_minute); Serial.print(":"); 
+  Serial.print(current_second); Serial.print(", "); 
+  Serial.print(voltage); Serial.print(", "); 
+  Serial.print(current_mA); Serial.print(", ");
+  Serial.println(power_mW);
 }
